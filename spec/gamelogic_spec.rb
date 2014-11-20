a3b9497543dcd6ab77cbe5395dea5e9d0eb30f25
William Gallagher <wgallagher86@gmail.com>
2014-11-20 14:49:40 -0800

    Add view card page
    -Shows card question
    -Does not yet have a form for answer

--- spec/gamelogic_spec.rb
+++ spec/gamelogic_spec.rb
@@ -2,9 +2,11 @@ require_relative 'spec_helper'
 
 describe "Game Logic" do
   describe "We can see a specific card" do
+    it "shows the card title" do
     @card = Card.create(question: "Is this is a test", answer: "yes")
-    get "cards/1"
+    get "/cards/1"
     expect(last_response.body).to include(@card.question)
+  end
   end
 end
 
