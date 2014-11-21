7d14ff52bcd0353bdcdad561ecfb69971dc468fe
Nick Perez <nickperez1285@gmail.com>
2014-11-21 11:31:02 -0800

    nothing changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,13 @@ get '/games/new' do
   erb :"/games/new"
 end
 
+get '/decks/:deck_id/cards'
+#shows all cards
+end
+
+
+get'game/:game_id/
+
 get '/decks/:deck_id/cards/:card_id' do
  @deck = Deck.find(params[:deck_id])
  @card1 = @deck.cards[1]  #### must change to random card
