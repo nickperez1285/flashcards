1f1579811b2b4b8e867fdff923ff9bc6d8a99ad4
Han Kim <hankim813@gmail.com>
2014-11-21 10:53:58 -0800

    refactor seeds.rb file for populating decks and cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,7 @@
 require 'faker'
-3.times do
-  Deck.create(title: Faker::Commerce.department)
+
+10.times do
+  deck = Deck.create(title: Faker::Commerce.department)
+  5.times { deck.cards << Card.create(question: Faker::Lorem.sentence, answer: "answer") }
 end
 
-5.times do
-  Card.create(question: Faker::Lorem.sentence, answer: "answer", deck_id: 1)
-end
-
-5.times do
-  Card.create(question: Faker::Lorem.sentence, answer: "answer", deck_id: 2)
-end
-
-5.times do
-  Card.create(question: Faker::Lorem.sentence, answer: "answer", deck_id: 3)
-end
