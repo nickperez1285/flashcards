aa1ba5b96082f9391fc976e9c0c8502c6b471044
William Gallagher <wgallagher86@gmail.com>
2014-11-21 11:31:55 -0800

    worked on compare

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,17 @@
 get '/cards/:id' do
   @card = Card.find(params[:id])
-  erb :display_card
+  @counter = 1
+  erb :"cards/display_card"
+end
+
+get '/cards/:id/compare' do
+  @counter = 0
+  @card = Card.find(params[:id])
+  @submitted_answer = (params[:submitted_answer])
+  erb :"cards/compare"
+  if @card.answer == @submitted_answer
+    redirect "/cards/#{@card.id + 1}"
+  else
+    redirect "/cards/#{@card.id}"
+  end
 end
