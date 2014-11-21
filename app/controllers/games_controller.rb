be80f681ba0bd1efa5cd5ba734a5056da4a3a0f4
Han Kim <hankim813@gmail.com>
2014-11-21 10:24:12 -0800

    add basic css layout

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 get '/games/new' do
   # render the new game/menu page
   if current_user
-    @message = "Successfully logged in."
+    @message = ["Intelligence is a more accurate predictor of future success than socioeconomic background.", "Intelligence isn’t static.", "IQ scores can make you more confident, or less so.", "Not all intelligence ages the same.", "You can make yourself smarter but only if you believe you can.","Men and women demonstrate equal intelligence.", "There’s more than one way to be smart.", "You can learn to improve your memory.", "New brain connections are created every time you form a memory.", "A good night's sleep may improve your memory."].sample
     @decks = Deck.all
     @user = current_user
     erb :"/games/new"
