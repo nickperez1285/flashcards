551396a251af24ab98332879cbadfe178f0d7b09
William Gallagher <wgallagher86@gmail.com>
2014-11-21 16:54:18 -0800

    added bass trivia

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
   deck1 = Deck.create(title: "Beers & Books")
   deck2= Deck.create(title: "Num Nums")
   deck3= Deck.create(title: "DBC 123")
+  deck4= Deck.create(title: "Largemouth Bass Trivia")
 
 
  deck1.cards << Card.create(question: "This tasty beer is a brown ale from England", answer:"new castle" )
@@ -26,3 +27,26 @@
  deck3.cards << Card.create(question: "SUDOCODE! teacher", answer: "brick" )
  deck3.cards << Card.create(question: "Rides an ecofriendly one wheeled segway", answer: "topher" )
  deck3.cards << Card.create(question: " Who is your daddy and what does he do ?", answer: "arnold " )
+
+ deck4.cards << Card.create(question: "This Bass had a role in the TV show 'Silk Stockings'", answer: "neil")
+  deck4.cards << Card.create(question: "This Bass is a cycling enthusiest who live in Austin, Texas for a year.", answer: "sam")
+
+  deck4.cards << Card.create(question: "This bass taugh English in Korea", answer: "nick")
+
+  deck4.cards << Card.create(question: "This Bass was a mountain guide", answer:"joe")
+
+   deck4.cards << Card.create(question: "This Bass has a degree in Music", answer: "bill")
+
+   deck4.cards << Card.create(question: "This Bass has had a huge amount of pets", answer: 'randall')
+
+  deck4.cards << Card.create(question: "This Bass hasn't wiped in a disturbingly long time", answer: "chris")
+
+  deck4.cards << Card.create(question: "This Bass is a huge anime and counter-strike fan", answer: "eddie")
+
+  deck4.cards << Card.create(question: "This Bass does not have an 'r' in his last name", answer: 'ryan')
+
+  deck4.cards << Card.create(question: "This Bass will not eat chicken and eggs in the same meal", answer: "valerie")
+
+
+
+
